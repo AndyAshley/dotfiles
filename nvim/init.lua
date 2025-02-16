28c95d8712ee5ef15e4bdf5e09d180a642604821
Andy Ashley <your-email@example.com>
2025-02-15 19:07:10 -0500

    init dotfiles setup

---
+++ nvim/init.lua
@@ -0,0 +1,15 @@
+-- Set tab width
+vim.opt.tabstop = 4        -- Number of spaces a tab represents
+vim.opt.shiftwidth = 4     -- Number of spaces for autoindent
+vim.opt.expandtab = true   -- Convert tabs to spaces instead of using tab characters
+
+-- Enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable syntax highlighting
+vim.opt.syntax = "on"
+
+-- Enable mouse support
+vim.opt.mouse = "a"
+
